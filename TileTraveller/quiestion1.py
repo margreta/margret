d8d8764b620e01b7ba567580bb8ab3e3646eca57
margreta <margreta11@ru.is>
2018-09-19 09:04:13 +0000

    minor changes

--- TileTraveller/quiestion1.py
+++ TileTraveller/quiestion1.py
@@ -9,35 +9,36 @@ WEST = "w"
 while x != 3 or y != 1:
     if x == 1 and y == 1:
         possible_move = NORTH
-        output1 = "(N)orth"
+        output1 = "(N)orth."
     elif x == 1 and y == 2: 
         possible_move = NORTH, EAST, SOUTH
-        output1= "(N)orth or (E)ast or (S)outh"
+        output1= "(N)orth or (E)ast or (S)outh."
     elif x == 1 and y == 3:
         possible_move = EAST, SOUTH
-        output1 = "(E)ast, (S)outh"
+        output1 = "(E)ast or (S)outh."
     elif x == 2 and y == 1:
         possible_move = NORTH
-        output1 = "(N)orth"
+        output1 = "(N)orth."
     elif x == 2 and y == 2:
         possible_move = SOUTH, WEST
-        output1 = "(S)outh or (W)est"
+        output1 = "(S)outh or (W)est."
     elif x == 2 and y == 3: 
         possible_move = EAST, WEST
-        output1 = "(E)ast or (W)est"
+        output1 = "(E)ast or (W)est."
     elif x == 3 and y == 3:
         possible_move = WEST, SOUTH
-        output1 = "(W)est, (S)outh"
+        output1 = "(S)outh or (W)est."
     elif x == 3 and y == 2:
         possible_move = NORTH, SOUTH
-        output1 = "(N)orth or (S)outh"
+        output1 = "(N)orth or (S)outh."
 
 
     print("You can travel: {}".format(output1))
     move = input("Direction: ")
     
-    if move.lower() not in [NORTH,SOUTH,EAST,WEST]:
+    if move.lower() not in possible_move:
         print("Not a valid direction!")
+        continue
 
     if move.lower() == NORTH:
         y += 1
